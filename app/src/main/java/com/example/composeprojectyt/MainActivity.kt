91cd3daa9ecea41aaf170c8bbafa65c2d28ddaac
Sergei Krainyukov <sk-new@mail.ru>
2022-07-17 11:34:24 +0300

    Lesson6

--- app/src/main/java/com/example/composeprojectyt/MainActivity.kt
+++ app/src/main/java/com/example/composeprojectyt/MainActivity.kt
@@ -3,10 +3,58 @@ package com.example.composeprojectyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        setContent {
+            val color = remember {
+                mutableStateOf(Color.Yellow)
+            }
+            Column(Modifier.fillMaxSize()) {
+                ColorBox(
+                    Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color.value)
+                        .weight(1f)
+                )
+            }
+
+        }
     }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier, updColor: (Color) -> Unit) {
+    Box(modifier = modifier
+        .background(Color.Black)
+        .clickable {
+            updColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                )
+            )
+        }
+    )
 }
