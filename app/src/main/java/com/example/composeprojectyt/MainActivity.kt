4c01b7ff7abe9d4c7ccb876474a783d2c3fee7b4
Sergei Krainyukov <sk-new@mail.ru>
2022-07-17 14:31:02 +0300

    Lesson10

--- app/src/main/java/com/example/composeprojectyt/MainActivity.kt
+++ app/src/main/java/com/example/composeprojectyt/MainActivity.kt
@@ -3,10 +3,33 @@ package com.example.composeprojectyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.produceState
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        setContent {
+            val scaffoldState = rememberScaffoldState()
+            Scaffold(scaffoldState = scaffoldState) {
+                val counter = produceState(initialValue = 0) {
+                    delay(3000)
+                    value = 5
+                }
+                if (counter.value == 5) {
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                        scaffoldState.snackbarHostState.showSnackbar("Hello!")
+                    }
+                }
+                Button(onClick = {}) {
+                    Text(text = "Click Here: ${counter.value}")
+                }
+            }
+        }
     }
 }
