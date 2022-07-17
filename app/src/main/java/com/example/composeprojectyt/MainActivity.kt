97bfc285ab7eadc8145216e04e9560f199b8033b
Sergei Krainyukov <sk-new@mail.ru>
2022-07-17 12:42:31 +0300

    Lesson8

--- app/src/main/java/com/example/composeprojectyt/MainActivity.kt
+++ app/src/main/java/com/example/composeprojectyt/MainActivity.kt
@@ -3,10 +3,31 @@ package com.example.composeprojectyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        setContent {
+            LazyColumn {
+                items(1000) {
+                    Text(
+                        text = "$it",
+                        modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
     }
 }
