95a9f1f536723c084e00f8704734a76bdbde0c9c
Sergei Krainyukov <sk-new@mail.ru>
2022-07-17 11:05:42 +0300

    Lesson5

--- app/src/main/java/com/example/composeprojectyt/MainActivity.kt
+++ app/src/main/java/com/example/composeprojectyt/MainActivity.kt
@@ -3,10 +3,58 @@ package com.example.composeprojectyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        setContent {
+            val fontFamily = FontFamily(
+                Font(R.font.roboto_bold, FontWeight.Bold),
+                Font(R.font.roboto_medium, FontWeight.Medium),
+                Font(R.font.roboto_regular, FontWeight.Normal)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Black)
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)) {
+                            append("J")
+                        }
+                        append("etpack")
+                        withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)) {
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
+            }
+
+        }
     }
 }
