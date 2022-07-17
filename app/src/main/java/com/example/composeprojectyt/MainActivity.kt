e520adfc466f23a98e0bc20e6d77f339df554312
Sergei Krainyukov <sk-new@mail.ru>
2022-07-17 12:03:42 +0300

    Lesson7

--- app/src/main/java/com/example/composeprojectyt/MainActivity.kt
+++ app/src/main/java/com/example/composeprojectyt/MainActivity.kt
@@ -3,10 +3,48 @@ package com.example.composeprojectyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        setContent {
+            val scaffoldState = rememberScaffoldState()
+            var textFieldStateValue by remember {
+                mutableStateOf("")
+            }
+            val buttonScope = rememberCoroutineScope()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    TextField(
+                        value = textFieldStateValue,
+                        label = { Text("Enter your name") },
+                        singleLine = true,
+                        onValueChange = {
+                            textFieldStateValue = it
+                        })
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        buttonScope.launch { scaffoldState.snackbarHostState.showSnackbar("Hello, $textFieldStateValue") }
+                    }) {
+                        Text("Press Me")
+                    }
+                }
+            }
+        }
     }
 }
